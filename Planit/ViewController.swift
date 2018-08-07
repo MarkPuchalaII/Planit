b6a84f6dcecfaf88756fe86669a97be1b6a28dcd
Mark Belain <markPuchalaii@gmail.com>
2018-08-07 14:21:26 -0500

    Daily Push - Removed "tutorial Button"
    Had no use for that "tutorial button" in the app, so I did away with it.
    
    I need to reorganize the checklist. I don't know if I'll do that manually or build an option for it into the app.
    
    I think I'd be better off building the option first.

---
+++ Planit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Planit
+//
+//  Created by Mark Puchala II on 8/6/18.
+//  Copyright © 2018 Mark Puchala II. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

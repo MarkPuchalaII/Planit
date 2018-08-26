ba2254773f6d2d66bd33978618f53c398dd43510
Mark Belain <markPuchalaii@gmail.com>
2018-08-26 00:19:30 -0500

    Daily Push
    I'm getting that much closer to daily coding projects.
    Currently have a lot to read, though.

--- Daily Agenda/calc.py
+++ Daily Agenda/calc.py
@@ -1,6 +1,9 @@
-print(2.5 / 2.5, "cups of water")
-print(1 / 2.5, " TSP Sugar")
-print(2 / 2.5, " TSP Yeast")
-print(7 / 2.5, "cups of Flour")
-print(1.5 / 2.5, "TSP Salt")
-print(6 / 2.5, "TBSP Olive Oil")
+i = 0.25
+print(2.5 * i, "cups of water")
+print(1 * i, " TSP Sugar")
+print(2 * i, " TSP Yeast")
+print(7 * i, "cups of Flour")
+print(1.5 * i, "TSP Salt")
+print(6 * i, "TBSP Olive Oil")
+
+2.5/3

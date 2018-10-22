feea05ea9e8076f91671f88b6d3cc9772e6a3e53
Mark Belain <markPuchalaii@gmail.com>
2018-10-21 19:19:18 -0500

    Daily Push
    Back to work again
    Gosh, this looks like a vacation when it was really a wonderful disruption.

--- Daily Agenda/calc.py
+++
@@ -1,9 +0,0 @@
-i = 0.25
-print(2.5 * i, "cups of water")
-print(1 * i, " TSP Sugar")
-print(2 * i, " TSP Yeast")
-print(7 * i, "cups of Flour")
-print(1.5 * i, "TSP Salt")
-print(6 * i, "TBSP Olive Oil")
-
-2.5/3
